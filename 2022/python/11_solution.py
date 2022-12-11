c1e1fe4b11c372412a97f5e14df1bfb70bb6a86a
Patch Neranartkomol <patch@neranartkomol.com>
2022-12-10 23:54:24 -0800

    DRY parsing and update typehint

--- 2022/python/11_solution.py
+++ 2022/python/11_solution.py
@@ -25,7 +25,7 @@ def execute_round(monkeys: list[Monkey]) -> None:
         m.items.clear()
 
 
-def execute_p2_round(monkeys: list[Monkey], factor) -> None:
+def execute_p2_round(monkeys: list[Monkey], factor: int) -> None:
     for m in monkeys:
         for item in m.items:
             m.inspection_count += 1
@@ -52,38 +52,34 @@ def function_factory(op_str: str) -> Callable:
             raise RuntimeError("Unexpected Monkey Inspection operator")
 
 
-monkeys = []
-with open('../input/11_input.txt', 'r', encoding='utf-8') as f:
-    lines = [line.strip('\n') for line in f.readlines()]
-    for i in range(0, len(lines), 7):
-        starting_items = list(map(int, lines[i + 1].split(': ')[-1].split(', ')))
-        f = function_factory(lines[i + 2].split(": ")[-1])
-        test = int(lines[i + 3].split(' ')[-1])
-        succeed = int(lines[i + 4].split(' ')[-1])
-        fail = int(lines[i + 5].split(' ')[-1])
-        monkeys.append(Monkey(starting_items, f, test, succeed, fail, 0))
-    
-    for _ in range(20):
-        execute_round(monkeys)
-    monkeys.sort(key=lambda m: m.inspection_count, reverse=True)
-    mb = monkeys[0].inspection_count * monkeys[1].inspection_count
-    print(f'Part 1: Monkey Business {mb}')
+def read_input(monkeys: list[Monkey]) -> None:
+    with open('../input/11_input.txt', 'r', encoding='utf-8') as f:
+        lines = [line.strip('\n') for line in f.readlines()]
+        for i in range(0, len(lines), 7):
+            starting_items = list(map(int, lines[i + 1].split(': ')[-1].split(', ')))
+            f = function_factory(lines[i + 2].split(": ")[-1])
+            test = int(lines[i + 3].split(' ')[-1])
+            succeed = int(lines[i + 4].split(' ')[-1])
+            fail = int(lines[i + 5].split(' ')[-1])
+            monkeys.append(Monkey(starting_items, f, test, succeed, fail, 0))
+
 
 monkeys = []
-with open('../input/11_input.txt', 'r', encoding='utf-8') as f:
-    lines = [line.strip('\n') for line in f.readlines()]
-    for i in range(0, len(lines), 7):
-        starting_items = list(map(int, lines[i + 1].split(': ')[-1].split(', ')))
-        f = function_factory(lines[i + 2].split(": ")[-1])
-        test = int(lines[i + 3].split(' ')[-1])
-        succeed = int(lines[i + 4].split(' ')[-1])
-        fail = int(lines[i + 5].split(' ')[-1])
-        monkeys.append(Monkey(starting_items, f, test, succeed, fail, 0))
+read_input(monkeys)
+   
+for _ in range(20):
+    execute_round(monkeys)
+monkeys.sort(key=lambda m: m.inspection_count, reverse=True)
+mb = monkeys[0].inspection_count * monkeys[1].inspection_count
+print(f'Part 1: Monkey Business {mb}')
 
-    # Find common factor in modulo tests for all monkeys
-    factor = reduce(mul, (m.test for m in monkeys), 1)
-    for _ in range(10000):
-        execute_p2_round(monkeys, factor)
-    monkeys.sort(key=lambda m: m.inspection_count, reverse=True)
-    mb = monkeys[0].inspection_count * monkeys[1].inspection_count
-    print(f'Part 2: Monkey Business {mb}')
+monkeys = []
+read_input(monkeys)
+
+# Find common factor in modulo tests for all monkeys
+factor = reduce(mul, (m.test for m in monkeys), 1)
+for _ in range(10000):
+    execute_p2_round(monkeys, factor)
+monkeys.sort(key=lambda m: m.inspection_count, reverse=True)
+mb = monkeys[0].inspection_count * monkeys[1].inspection_count
+print(f'Part 2: Monkey Business {mb}')
